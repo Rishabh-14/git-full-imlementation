1613c61c0ae5bfc336fe6d8335853fa91c0511c9
Rishabh-14 <77384078+Rishabh-14@users.noreply.github.com>
2023-07-25 13:03:41 +0530

    tree_object.py implementation complete

--- tree_object.py
+++ tree_object.py
@@ -14,3 +14,25 @@ def read_tree(repo_path, tree_sha):
 
     null_byte_index = object.content.index(b'x\00')
     object_content = decompressed_data[null_byte_index+1:]
+
+    tree = []
+
+    while object_content:
+        space_index = object_content.index(b' ')
+        file_mode = object_content[:space_index].decode("utf-8")
+
+        null_byte_index = object_content.index(b'x\00',space_index)
+        name = object_content[:space_index+1: null_byte_index].decode('utf-8')
+
+        sha1 = object_content[null_byte_index+1:null_byte_index+21]
+        sha1_hex = sha1.hex()
+
+        tree.append((file_mode, name, sha1_hex))
+        object_content = object_content[null_byte_index + 21]
+
+    return tree
+
+# usage
+repo_path = "/path/to/your/repo"  # replace with your repository path
+tree_sha = "your_tree_sha"  # replace with your tree SHA
+print(read_tree(repo_path, tree_sha))
